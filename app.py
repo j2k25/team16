109aed3fa2c921b8532ea4367268abdadc6dcf4e
j2k25 <jjohnson4575@gmail.com>
2020-04-10 16:44:25 -0500

    insert magic

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
+import re
 import MySQLdb.cursors
 import mysql.connector
 from flask import Flask, render_template, request, redirect, url_for, session
 from flask_mysqldb import MySQL
-import re
 
 app = Flask(__name__)
 
@@ -31,73 +31,22 @@ def login():
         password = request.form['password']
         # Check if account exists using MySQL
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute('SELECT * FROM employee_accounts WHERE username = %s AND password = %s', (username, password,))
+        cursor.execute('SELECT * FROM accounts WHERE username = %s AND password = %s', (username, password,))
         # Fetch one record and return result
         account = cursor.fetchone()
-        # If account exists in accounts table in our database
+        # If account exists in accounts table in out database
         if account:
             # Create session data, we can access this data in other routes
             session['loggedin'] = True
             session['id'] = account['id']
             session['username'] = account['username']
-            session['is_manager'] = account['is_manager']
-            if session['is_manager'] == 1:
-                # Redirect to manager home page
-                return render_template('manager_home.html', msg=msg)
-            else:
-                if session['is_manager'] == 0:
-                    # Redirect to employee home page
-                    return render_template('employee_home.html', msg=msg)
+            # Redirect to home page
+            return render_template('home.html', msg=msg)
         else:
             # Account doesnt exist or username/password incorrect
             msg = 'Incorrect username/password!'
     # Show the login form with message (if any)
     return render_template('index.html', msg=msg)
-
-
-# http://localhost:5000/pythinlogin/insert - this will be the registration page, we need to use both GET and POST requests
-@app.route('/insert_account', methods=['GET', 'POST'])
-def insert_account():
-    # Output message if something goes wrong...
-    msg = ''
-    # Check if "username", "password" and "email" POST requests exist (user submitted form)
-    if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form:
-        # Create variables for easy access
-        username = request.form['username']
-        password = request.form['password']
-        email = request.form['email']
-
-        # Check if account exists using MySQL
-        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute('SELECT * FROM accounts WHERE username = %s', (username,))
-        account = cursor.fetchone()
-        # If account exists show error and validation checks
-        if account:
-            msg = 'Account already exists!'
-        elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
-            msg = 'Invalid email address!'
-        elif not re.match(r'[A-Za-z0-9]+', username):
-            msg = 'Username must contain only characters and numbers!'
-        elif not username or not password or not email:
-            msg = 'Please fill out the form!'
-        else:
-            # Account doesnt exists and the form data is valid, now insert new account into accounts table
-            cursor.execute('INSERT INTO accounts VALUES (NULL, %s, %s, %s)', (username, password, email,))
-            mysql.connection.commit()
-            msg = 'You have successfully registered!'
-
-
-    # Check if user is loggedin
-    elif 'loggedin' in session:
-        if request.method == 'POST':
-            # Form is empty... (no POST data)
-            msg = 'Please fill out the form!'
-            return render_template('insert_account.html', msg=msg)
-            # Show registration form with message (if any)
-            return render_template('insert_account.html')
-    else:
-        return redirect(url_for('login'))
-
 
 
 @app.route('/timestamp')
@@ -127,31 +76,8 @@ def logout():
 def home():
     # Check if user is loggedin
     if 'loggedin' in session:
-        if session['is_manager'] == 1:
-            return render_template('manager_home.html', username=session['username'])
-        else:
-            if session['is_manager'] == 0:
-                return render_template('employee_home.html', username=session['username'])
-    # User is not loggedin redirect to login page
-    return redirect(url_for('login'))
-
-
-@app.route('/employee_home')
-def employee_home():
-    # Check if user is loggedin
-    if 'loggedin' in session:
         # User is loggedin show them the home page
-        return render_template('employee_home.html', username=session['username'])
-    # User is not loggedin redirect to login page
-    return redirect(url_for('login'))
-
-
-@app.route('/manager_home')
-def manager_home():
-    # Check if user is loggedin
-    if 'loggedin' in session:
-        # User is loggedin show them the home page
-        return render_template('manager_home.html', username=session['username'])
+        return render_template('home.html', username=session['username'])
     # User is not loggedin redirect to login page
     return redirect(url_for('login'))
 
@@ -165,7 +91,7 @@ def profile():
     if 'loggedin' in session:
         # We need all the account info for the user so we can display it on the profile page
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute('SELECT * FROM employee_accounts WHERE id = %s', (session['id'],))
+        cursor.execute('SELECT * FROM accounts WHERE id = %s', (session['id'],))
         account = cursor.fetchone()
         # Show the profile page with account info
         return render_template('profile.html', account=account)
@@ -179,12 +105,50 @@ def reports():
     if 'loggedin' in session:
         # We need all the account info for the user so we can display it on the profile page
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
-        cursor.execute('SELECT * FROM employee_accounts WHERE id = %s', (session['id'],))
+        cursor.execute('SELECT * FROM accounts WHERE id = %s', (session['id'],))
         account = cursor.fetchone()
         # Show the profile page with account info
         return render_template('reports.html', account=account)
     # User is not loggedin redirect to login page
     return redirect(url_for('login'))
+
+
+# http://localhost:5000/pythinlogin/insert - this will be the registration page, we need to use both GET and POST requests
+@app.route('/insert', methods=['GET', 'POST'])
+def insert():
+    # Output message if something goes wrong...
+    msg = ''
+    # Check if "username", "password" and "email" POST requests exist (user submitted form)
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form:
+        # Create variables for easy access
+        username = request.form['username']
+        password = request.form['password']
+        email = request.form['email']
+
+        # Check if account exists using MySQL
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
+        cursor.execute('SELECT * FROM accounts WHERE username = %s', (username,))
+        account = cursor.fetchone()
+        # If account exists show error and validation checks
+        if account:
+            msg = 'Account already exists!'
+        elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+            msg = 'Invalid email address!'
+        elif not re.match(r'[A-Za-z0-9]+', username):
+            msg = 'Username must contain only characters and numbers!'
+        elif not username or not password or not email:
+            msg = 'Please fill out the form!'
+        else:
+            # Account doesnt exists and the form data is valid, now insert new account into accounts table
+            cursor.execute('INSERT INTO accounts VALUES (NULL, %s, %s, %s)', (username, password, email,))
+            mysql.connection.commit()
+            msg = 'You have successfully registered!'
+
+    elif request.method == 'POST':
+        # Form is empty... (no POST data)
+        msg = 'Please fill out the form!'
+    # Show registration form with message (if any)
+    return render_template('insert.html', msg=msg)
 
 
 if __name__ == '__main__':
